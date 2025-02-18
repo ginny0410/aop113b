3915846b68340b3555c37c8ee89a3809a5e0925d
ginny0410 <83101932+ginny0410@users.noreply.github.com>
2025-02-18 16:02:42 +0800

    Add files via upload

---
+++ EX01-01加法器.py
@@ -0,0 +1,3 @@
+num1= int(input('請輸入第一個數字: '))
+num2 = int(input('請輸入第二個數字: '))
+print('result: ', num1 + num2)
---
+++ EX01-02BMI計算.py
@@ -0,0 +1,3 @@
+weight = float(input('請輸入體重: '))
+height = float(input('請輸入身高: '))
+print(round(weight/height**2,1))
